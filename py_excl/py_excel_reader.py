9c463249df09bafbc5cb8f294acc26536beb108b
Administrator <lichao8050@outlook.com>
2022-09-15 22:14:36 +0800

    提交关于表格12212

--- py_excl/py_excel_reader.py
+++ py_excl/py_excel_reader.py
@@ -4,6 +4,7 @@
 # @FileName :
 # 用代码读取Excel
 from py_excl.read_excel import ExcelReader
+import requests
 
 excel = ExcelReader(r"G:\pytho\py_excl\test_excel.xlsx")
 count = excel.get_case_count()
@@ -11,6 +12,17 @@ print(count)
 excel.close_file()
 # 循环遍历所有用例
 for row in range(0, count): #  循环语句   row是变量  范围是0到count
-    api_case_name = excel.get_test_case_name(row)
-    api_method = excel.get_method(row)
+    api_case_name = excel.get_test_case_name(row) # 读取用例名字
+    api_method = excel.get_method(row) # 读取用例请求方式
+    api_url = excel.get_url(row) # 读取用例请求URL
+    api_params = excel.get_headers(row) # 读取用例请求数据
+    api_data = excel.get_test_data(row) # 读取用例请求数据
     print(api_case_name, api_method)
+    if api_method == 'get':
+        res = requests.get(
+            url=api_url,
+            params=api_params
+        )
+        print(res.json())
+    if api_method == 'post':
+        requests.post()
